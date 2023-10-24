b95b50aae1abc05c938982f77c72493a915c4fb6
Prakhar Agarwal <realprakhar@outlook.com>
2023-10-24 19:37:34 +0530

    Initial Commit

---
+++ FructusApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FructusApp
+//
+//  Created by Prakhar Agarwal on 24/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FructusApp/FructusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusAppApp.swift
+//  FructusApp
+//
+//  Created by Prakhar Agarwal on 24/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
